be9f72f854477efad618cc2fe20a876b8f902564
Fateh <fateh137@gmail.com>
2019-09-03 15:25:25 +0100

    first change

--- src/App.js
+++ src/App.js
@@ -12,11 +12,11 @@ function App() {
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://malekbenz.com/"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          MalekBenz Site
         </a>
       </header>
     </div>
