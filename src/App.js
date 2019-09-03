c9652c3b8927847b069154c49e4172238653d571
Fateh <fateh137@gmail.com>
2019-09-03 16:22:33 +0100

    creation first component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import MyLink from './MyLink' ;
 
 function App() {
   return (
@@ -16,7 +17,9 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          MalekBenz Site AHMED kkkkk
+         <MyLink name ="Fateh" />
+         <MyLink name ="Ahmed" />
+          
         </a>
       </header>
     </div>
---
+++ src/MyLink.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+
+function MyLink(props) {
+  return (
+   <div>
+    <h1>{props.name}</h1>
+   </div>
+  );
+}
+
+export default MyLink;
