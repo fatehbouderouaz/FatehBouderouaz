7e4c9bea2f10145f264b0fbbd99affe90d82fea8
Fateh <fateh137@gmail.com>
2019-09-20 17:51:49 +0100

    search bar css

--- src/Blog.js
+++ src/Blog.js
@@ -4,7 +4,8 @@ import axios from "axios";
 class Blog extends React.Component {
   state = {
     text: "محمد",
-    response: []
+    response: [],
+    count: 0
   };
 
   handleChange = e => {
@@ -17,57 +18,64 @@ class Blog extends React.Component {
   handleSearch = e => {
     const mytext = this.state.text;
     axios
-      .get(
-        `//api.alquran.cloud/v1/search/${mytext}/all/quran-simple-clean`
-      )
+      .get(`//api.alquran.cloud/v1/search/${mytext}/all/quran-simple-clean`)
 
       .then(res => {
-        //console.log(res.data.data.matches);
+        console.log(res.data.data.count);
         this.setState({
-          response: res.data.data.matches
+          response: res.data.data.matches,
+          count: res.data.data.count
         });
       });
-
-      
   };
 
   render() {
     const { text } = this.state;
     return (
-      <div>
-        <h2>Search</h2>
-        <div>
+      <div className="container">
+    <div className="row searchBar">
+        <div className='col-xs-3 col-sm-3 col-md-3 col-lg-3'>
+          {this.state.count ? " نتائج : " + this.state.count : null}
+        </div>
+        <div className='col-xs-6 col-sm-6 col-md-6 col-lg-6'>
           <div className='input-group'>
             <input
               type='text'
-              className='form-control'
+              className='form-control textSearch'
               name='text'
               value={text}
               onChange={this.handleChange}
-              placeholder='Search'
+              placeholder='...بحث'
             />
             <span className='input-group-btn'>
               <button
                 type='button'
                 className='btn btn-default'
                 onClick={this.handleSearch}>
-                Go!
+                إبحث
               </button>
             </span>
           </div>
+        </div>
 
-          <div>
+        <div className='col-xs-3 col-sm-3 col-md-3 col-lg-3'>
+            
+        </div>
+    </div>
+
+          <div className="row">
             {this.state.response.map(respons => (
               <div key={respons.number} className='row'>
-                <div className='searchResponse'>{respons.surah.name} - {respons.numberInSurah}</div>
-                <div className='searchResponse'>{respons.text}</div>
+                <div className='searchResponse'>
+                  {respons.surah.name} - {respons.numberInSurah}
+                </div>
+                <div className='searchResponse'>{respons.text}
+                </div>
               </div>
             ))}
           </div>
-          
-        </div>
-      </div>
-    );
+          </div>
+    )
   }
 }
 
